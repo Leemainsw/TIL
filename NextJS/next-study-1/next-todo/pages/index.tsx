4f9abfe052a3538d16d2a64a769386db6102c2d7
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-10-12 16:16:26 +0900

    [NextJS] 클론 코딩으로 시작하는 Next.js 챕터 4 - 스타일드 컴포넌트 설치하기

--- NextJS/next-study-1/next-todo/pages/index.tsx
+++ NextJS/next-study-1/next-todo/pages/index.tsx
@@ -1,6 +1,24 @@
-import {NextPage} from 'next';
+import { NextPage } from 'next';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    paddingL 20px;
+`
+
 const index:NextPage = () => {
-    return <div>Hello Typescript</div>
+    return (
+        <Container>
+            <h1>Hello Styled-Component</h1>
+            <h2>Hello Styled-Component</h2>
+            <p>Hello Styled-Component</p>
+            <ul>
+                <li>hello Styled-components</li>
+            </ul>
+            <a>hello Styled-Components</a>
+            <span>hello Styled-components</span>
+        
+        </Container>
+    )
 }
 
 export default index;
