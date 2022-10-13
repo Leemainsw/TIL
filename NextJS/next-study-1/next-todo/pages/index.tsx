9b1352f4028cf04922868426fea6e4aa8ab1a11a
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-10-13 14:02:02 +0900

    [NextJS] 클론 코딩으로 시작하는 Next.js 챕터 6 - 환경변수(env) 설정하기

--- NextJS/next-study-1/next-todo/pages/index.tsx
+++ NextJS/next-study-1/next-todo/pages/index.tsx
@@ -1,16 +1,20 @@
 import { GetServerSideProps, NextPage } from 'next';
 import TodoList from '../components/TodoList';
 import { getTodosAPI } from '../lib/api/todo';
+import { TodoType } from '../types/todo';
 
-const app: NextPage = () => {
-    return <TodoList todos={[]} />;
+interface IProps {
+    todos: TodoType[];
+}
+
+const app: NextPage<IProps> = ({todos}) => {
+    return <TodoList todos={todos} />;
 }
 
 export const getServerSideProps: GetServerSideProps = async() => {
     try{
         const { data } = await getTodosAPI();
-        console.log(data);
-        return { props: {}}
+        return { props: { todos: data }}
     }catch(e){
         console.log(e);
         return { props: {} };
