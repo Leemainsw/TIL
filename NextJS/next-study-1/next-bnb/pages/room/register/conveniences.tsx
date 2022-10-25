2b12f7c38ea795e8f632ae723530496511223662
Lee_yam <48716775+Leemainsw@users.noreply.github.com>
2022-10-25 19:39:42 +0900

    [NextJS] 클론 코딩으로 시작하는 Next.js 챕터 16 - 숙소 등록하기 6단계 (편의 공간) (#22)

---
+++ NextJS/next-study-1/next-bnb/pages/room/register/conveniences.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+import React from "react";
+import RegisterRoomConveniences from "../../../components/room/register/RegisterRoomConveniences";
+
+const conveniences: NextPage = () => {
+  return <RegisterRoomConveniences />;
+};
+
+export default conveniences;
