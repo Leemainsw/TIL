3aadd4cba8ef6420768560fe40d9c2740eab2c6a
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-10-25 15:41:11 +0900

    [NextJS] 클론 코딩으로 시작하는 Next.js 챕터 14 - 현재 위치 불러오기

--- NextJS/next-study-1/next-bnb/components/room/register/RegisterRoomLocation.tsx
+++ NextJS/next-study-1/next-bnb/components/room/register/RegisterRoomLocation.tsx
@@ -105,6 +105,19 @@ const RegisterLocation: React.FC = () => {
   const onChangePostcode = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(registerRoomActions.setPostcode(e.target.value));
   };
+
+  const onSuccessGetLocation = ({ coords }: { coords: {latitude: number, longitude: number} }) => {
+    console.log("latitude", coords.latitude);
+    console.log("longitude", coords.longitude);
+  };
+
+  const onClickGetCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(onSuccessGetLocation, (e) => {
+      console.log(e);
+      alert(e?.message);
+    });
+  };
+
   return (
     <Container>
       <h2>숙소의 위치를 알려주세요.</h2>
@@ -113,7 +126,7 @@ const RegisterLocation: React.FC = () => {
         정확한 숙소 주소는 게스트가 예약을 완료한 후에만 공개됩니다.
       </p>
       <div className="register-room-location-button-wrapper">
-        <Button color="dark_cyan" colorReverse icon={<NavigationIcon />}>현재 위치 사용</Button>
+        <Button color="dark_cyan" colorReverse icon={<NavigationIcon />} onClick={onClickGetCurrentLocation}>현재 위치 사용</Button>
       </div>
       <div className="register-room-location-country-selector-wrapper">
         <Selector
