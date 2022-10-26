1ea98144229f9d0bb3cabb9d8fd4a0b380859906
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-10-26 14:47:05 +0900

    [NextJS] 클론 코딩으로 시작하는 Next.js 챕터 17 - aws S3 사용 설정하기

--- NextJS/next-study-1/next-bnb/pages/api/files/upload.ts
+++ NextJS/next-study-1/next-bnb/pages/api/files/upload.ts
@@ -1,5 +1,8 @@
 import formidable from "formidable";
 import { NextApiRequest, NextApiResponse } from "next";
+import aws from "aws-sdk";
+import { createReadStream } from "fs";
+import { v4 as uuidv4 } from "uuid";
 
 export const config = {
   api: {
@@ -10,10 +13,29 @@ export const config = {
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     try {
-        const form = new formidable.IncomingForm();
-        form.parse(req, async (err, fields, files) => {
-            console.log(files);
+      const form = new formidable.IncomingForm();
+      const url = await new Promise((resolve, reject) => {
+        form.parse(req, async (err, fields, files: any) => {
+          const s3 = new aws.S3({
+            accessKeyId: process.env.ACCESSKEY_ID,
+            secretAccessKey: process.env.SECRET_ACCESSKEY_ID,
+          });
+          const stream = createReadStream(files.file.filepath);
+          const originalFileName = files.file.originalFilename.split(".").shift();
+          const fileExtension = files.file.originalFilename.split(".").pop();
+          await s3.upload({
+            Bucket: process.env.S3_BUCKET_NAME!,
+            Key: `${originalFileName}__${uuidv4()}.${fileExtension}`,
+            ACL: "public-read",
+            Body: stream,
+          })
+          .promise()
+          .then((res) => console.log(res))
+          .catch((e) => console.log(e));
+          console.log(files);
         });
+      });
+      console.log("url", url);
     } catch (e) {
       console.log(e);
       res.end();
