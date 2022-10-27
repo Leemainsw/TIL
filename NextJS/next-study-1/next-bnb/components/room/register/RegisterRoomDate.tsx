b374644abad082b9cea65164cb376c4fbe80b42a
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-10-27 22:40:19 +0900

    [NextJS] 클론 코딩으로 시작하는 Next.js 챕터 21 - 공통 DatePicker 적용하기

--- NextJS/next-study-1/next-bnb/components/room/register/RegisterRoomDate.tsx
+++ NextJS/next-study-1/next-bnb/components/room/register/RegisterRoomDate.tsx
@@ -7,33 +7,88 @@ import palette from "../../../styles/palette";
 import DatePicker from "../../common/DatePicker";
 
 const Container = styled.div`
-  padding: 62px 30px 100px;
-  h2 {
-    font-size: 19px;
-    font-weight: 800;
-    margin-bottom: 56px;
+padding: 62px 30px 100px;
+h2 {
+  font-size: 19px;
+  font-weight: 800;
+  margin-bottom: 56px;
+}
+h3 {
+  font-size: 14px;
+  font-weight: bold;
+  color: ${palette.gray_76};
+  margin-bottom: 6px;
+}
+.register-room-date-wrapper {
+  display: flex;
+  align-items: center;
+  label {
+    span {
+      display: block;
+      margin-bottom: 8px;
+    }
   }
-
-  h3 {
-    font-weight: bold;
-    color: ${palette.gray_76};
-    margin-bottom: 6px;
+  input {
+    display: block;
+    position: relative;
+    width: 100%;
+    height: 46px;
+    padding: 0 11px;
+    border: 1px solid ${palette.gray_eb};
+    border-radius: 4px;
+    font-size: 16px;
+    outline: none;
+    & ::placeholder {
+      color: ${palette.gray_76};
+    }
+    & :focus {
+      border-color: ${palette.dark_cyan};
+    }
   }
+  .register-room-start-date {
+    margin-right: 20px;
+  }
+}
 `;
 
 const RegisterRoomDate: React.FC = () => {
   const startDate = useSelector((state) => state.registerRoom.startDate);
+  const endDate = useSelector((state) => state.registerRoom.endDate);
+
   const dispatch = useDispatch();
 
   const onChangeStartDate = (date: Date | null) => {
     dispatch(registerRoomActions.setStartDate(date ? date.toString() : null));
   };
 
+  const onChangeEndDate = (date: Date | null) => {
+    dispatch(registerRoomActions.setEndDate(date ? date.toString() : null));
+  };
+
   return (
     <Container>
       <h2>예약 가능 여부 설정하기</h2>
       <h3>11단계</h3>
-      <DatePicker selected={startDate ? new Date(startDate) : null} onChange={onChangeStartDate} />
+      <div className="register-room-date-wrapper">
+        <div className="register-room-start-date">
+          <label>
+            <span>예약 시작일</span>
+            <DatePicker
+              selected={startDate ? new Date(startDate) : null}
+              onChange={onChangeStartDate}
+            />
+          </label>
+        </div>
+        <div className="register-room-end-date">
+          <label>
+            <span>예약 마감일</span>
+            <DatePicker
+              selected={endDate ? new Date(endDate) : null}
+              onChange={onChangeEndDate}
+            />
+          </label>
+        </div>
+      </div>
     </Container>
   );
 };
