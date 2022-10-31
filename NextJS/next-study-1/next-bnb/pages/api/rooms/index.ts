40dd983c56ca20ee3100ed3ea9c71c567fc9be08
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-10-31 22:20:00 +0900

    [NextJS] 클론 코딩으로 시작하는 Next.js 챕터 24 - 숙소 리스트 불러오기 api

--- NextJS/next-study-1/next-bnb/pages/api/rooms/index.ts
+++ NextJS/next-study-1/next-bnb/pages/api/rooms/index.ts
@@ -4,6 +4,35 @@ import Data from "../../../lib/data";
 import { StoredRoomType } from "../../../types/room";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    const {
+      checkInDate,
+      checkOutDate,
+      adultCount,
+      childrenCount,
+      latitude,
+      longitude,
+      limit,
+      page = "1"
+    } = req.query;
+
+    try {
+      const rooms = Data.room.getList();
+      // 개수 자르기
+      const limitedRooms = rooms.splice(0 + (Number(page) - 1) * Number(limit), Number(limit));
+      // host 정보 넣기
+      const roomsWithHost = await Promise.all(
+        limitedRooms.map(async (room) => {
+          const host = Data.user.find({ id: room.hostId });
+          return { ...room, host };
+        })
+      );
+      res.statusCode = 200;
+      return res.send(roomsWithHost);
+    } catch (e) {
+      console.log(e);
+    }
+  }
   if (req.method === "POST") {
     try {
       const {
