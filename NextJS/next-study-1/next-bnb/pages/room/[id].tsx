114e5a80373ce2844fd3386f102516c9e64f60d7
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-02 18:32:02 +0900

    [NextJS] 클론 코딩으로 시작하는 Next.js 챕터 25 - 숙소 상세 스타일링

--- NextJS/next-study-1/next-bnb/pages/room/[id].tsx
+++ NextJS/next-study-1/next-bnb/pages/room/[id].tsx
@@ -1,9 +1,10 @@
 import { NextPage } from "next";
+import RoomDetail from "../../components/room/detail/RoomDetail";
 import { getRoomAPI } from "../../lib/api/room";
 import { roomActions } from "../../store/room";
 
 const roomDetail: NextPage = () => {
-  return <></>;
+  return <RoomDetail />;
 };
 
 roomDetail.getInitialProps = async ({ query, store }) => {
